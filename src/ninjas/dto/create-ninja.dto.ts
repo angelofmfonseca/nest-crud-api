9a93675b5606ef87cb36d8fd1545591368bef0c6
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2023-08-17 19:16:30 -0300

    feat: class validator weapon enum

--- src/ninjas/dto/create-ninja.dto.ts
+++ src/ninjas/dto/create-ninja.dto.ts
@@ -1,7 +1,9 @@
-import { MinLength } from 'class-validator';
+import { IsEnum, MinLength } from 'class-validator';
 
 export class CreateNinjaDto {
   @MinLength(3)
   name: string;
+
+  @IsEnum(['sword', 'stars', 'nunchucks'], { message: 'Use correct weapon' })
   weapon: string;
 }
